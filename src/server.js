a721d5b73a9fc558f8907abc710bc8cb0447e2ec
fadelkaadan <fadelkaadan@gmail.com>
2022-02-14 23:51:22 +0100

    test

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ server.use("/messages", messagesRoute);
 server.use("/censoredWords", censoredWordsRoute);
 
 server.all("/", (req, res) => {
-  res.send("Bot is running 3...");
+  res.send("Bot is running 4...");
 });
 
 export function start() {
